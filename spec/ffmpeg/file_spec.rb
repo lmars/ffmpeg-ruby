33a50917dfd50d26619e0fb0cdc790f65f1b9829
Lewis Marshall <lewismarshall86@gmail.com>
2013-10-05 03:39:27 +0100

    Test AVI files

--- spec/ffmpeg/file_spec.rb
+++ spec/ffmpeg/file_spec.rb
@@ -11,20 +11,43 @@ describe FFmpeg::File do
 
       its(:format_name) { should match(/^ASF/) }
     end
+
+    context "AVI" do
+      let(:filename) { "ruby-logo.h264.480x480.avi" }
+
+      its(:format_name) { should match(/^AVI/) }
+    end
   end
 
   describe "streams" do
-    let(:filename) { "ruby-logo.msmpeg4.480x480.wmv" }
-    let(:stream) do
-      {
-        "type"     => "video",
-        "codec"    => "msmpeg4v3",
-        "duration" => 1.0,
-        "width"    => 480,
-        "height"   => 480
-      }
+    context "WMV" do
+      let(:filename) { "ruby-logo.msmpeg4.480x480.wmv" }
+      let(:stream) do
+        {
+          "type"     => "video",
+          "codec"    => "msmpeg4v3",
+          "duration" => 1.0,
+          "width"    => 480,
+          "height"   => 480
+        }
+      end
+
+      its(:streams) { should == [stream] }
     end
 
-    its(:streams) { should == [stream] }
+    context "AVI" do
+      let(:filename) { "ruby-logo.h264.480x480.avi" }
+      let(:stream) do
+        {
+          "type"     => "video",
+          "codec"    => "h264",
+          "duration" => 1.0,
+          "width"    => 480,
+          "height"   => 480
+        }
+      end
+
+      its(:streams) { should == [stream] }
+    end
   end
 end
